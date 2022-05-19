36d409357ec324f63243a79a8fe781835fd231c8
John Estabrook <jestabro@vyos.io>
2022-05-19 14:57:51 -0500

    Initial setup

---
+++ memray/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="memray",
+    version="1.1.0",
+    author="VyOS maintainers and contributors",
+    author_email="maintainers@vyos.net",
+    python_requires=">=3.7",
+    license="BSD",
+    url="http://www.vyos.io",
+    packages=["memray"],
+    description=("A debian wrapper of memray."),
+)
